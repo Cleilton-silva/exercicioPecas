973806bb57d61eb95199b754e80d0f319a0a1a19
Cleillton-Silva <scleiilton@gmail.com>
2022-06-28 17:33:06 -0300

    primeiro commit

---
+++ pecas.js
@@ -0,0 +1,22 @@
+let pesoPeca = 160
+let numeroPecas = 7
+let NomePeca = "Roda"
+
+if (pesoPeca >= 100) {
+    console.log("pode-se cadastrar a peça")
+} else { 
+    console.log("peso insuficiente")
+}
+
+if (numeroPecas >=10) {
+    console.log("impossivel cadastrar, numero de peças excedido")
+} else {
+    console.log ("cadastro permitido")
+} 
+
+if (NomePeca.length < 3) {
+  console.log("numero de caracteres insuficiente")
+} else {
+    console.log("nome adequado de acordo com as diretivas")
+
+}
